c8c1ea399e18a427979db0427abc8382584b53fd
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2018-05-22 16:27:09 +0200

    Make sure we can test

--- src/kitta.ts
+++ src/kitta.ts
@@ -1,11 +1,1 @@
-"use strict";
-
-import * as Request from "request";
-
-Request.get("http://www.google.com", (error, response, body) => {
-    if (error) {
-        return;
-    }
-    console.log('🚀 statusCode:', response && response.statusCode);
-    console.log('🚀 body:', body);
-});
+export const value = "Kitta"
---
+++ tests/kitta.test.ts
@@ -0,0 +1,5 @@
+import { value } from "../src/kitta"
+
+it("value should be equal to Kitta", () => {
+  expect(value).toBe("Kitta")
+})
